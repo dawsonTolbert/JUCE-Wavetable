8913f2694721a819ca6b50bdf889de85e7c67b55
dawsonTolbert <tolbert_dawson@yahoo.com>
2024-03-03 17:55:30 -0500

    Implemented Wavetable Oscillator

--- Source/WavetableSynth.h
+++ Source/WavetableSynth.h
@@ -77,6 +77,46 @@ private:
     float currentAngle = 0.0f, angleDelta = 0.0f;
 };
 
+class WavetableOscillator
+{
+public:
+    WavetableOscillator(const juce::AudioSampleBuffer& wavetableToUse) : wavetable(wavetableToUse) 
+    {
+        jassert(wavetable.getNumChannels() == 1);
+    }
+
+    void setFrequency(float frequency, float sampleRate)
+    {
+        auto tableSizeOverSampleRate = (float)wavetable.getNumSamples() / sampleRate;
+        tableDelta = frequency * tableSizeOverSampleRate;
+    }
+
+    forcedinline float getNextSample() noexcept
+    {
+        auto tableSize = (unsigned int)wavetable.getNumSamples();
+
+        auto index0 = (unsigned int)currentIndex;
+        auto index1 = index0 == (tableSize - 1) ? (unsigned int)0 : index0 + 1;
+
+        auto frac = currentIndex - (float)index0;
+
+        auto* table = wavetable.getReadPointer(0);
+        auto value0 = table[index0];
+        auto value1 = table[index1];
+
+        auto currentSample = value0 + frac * (value1 - value0);
+
+        if ((currentIndex += tableDelta) > (float)tableSize) {
+            currentIndex -= (float)tableSize;
+        }
+
+        return currentSample;
+    }
+private:
+    const juce::AudioSampleBuffer& wavetable;
+    float currentIndex = 0.0f, tableDelta = 0.0f;
+};
+
 //==============================================================================
 class MainContentComponent   : public juce::AudioAppComponent,
                                public juce::Timer
@@ -88,6 +128,8 @@ public:
         cpuUsageText.setJustificationType (juce::Justification::right);
         addAndMakeVisible (cpuUsageLabel);
         addAndMakeVisible (cpuUsageText);
+
+        createWavetable();
 
         setSize (400, 200);
         setAudioChannels (0, 2); // no inputs, two outputs
@@ -111,13 +153,28 @@ public:
         cpuUsageText.setText (juce::String (cpu, 6) + " %", juce::dontSendNotification);
     }
 
+    void createWavetable()
+    {
+        sineTable.setSize(1, (int)tableSize);
+        auto* samples = sineTable.getWritePointer(0);
+
+        auto angleDelta = juce::MathConstants<double>::twoPi / (double)(tableSize - 1);
+        auto currentAngle = 0.0;
+
+        for (unsigned int i = 0; i < tableSize; ++i) {
+            auto sample = std::sin(currentAngle);
+            samples[i] = (float)sample;
+            currentAngle += angleDelta;
+        }
+    }
+
     void prepareToPlay (int, double sampleRate) override
     {
         auto numberOfOscillators = 200;                                                 // [1]
 
         for (auto i = 0; i < numberOfOscillators; ++i)
         {
-            auto* oscillator = new SineOscillator();                                    // [2]
+            auto* oscillator = new WavetableOscillator(sineTable);                                    // [2]
 
 
             //make list of possible note values in major chord
@@ -144,7 +201,7 @@ public:
             auto frequency = 440.0 * pow (2.0, (midiNote - 69.0) / 12.0);               // [4]
 
             oscillator->setFrequency ((float) frequency, (float) sampleRate);           // [5]
-            oscillators.add (oscillator);
+            oscillators.add(oscillator);
         }
 
         level = 0.25f / (float) numberOfOscillators;                                    // [6]
@@ -177,8 +234,11 @@ private:
     juce::Label cpuUsageLabel;
     juce::Label cpuUsageText;
 
+    const unsigned int tableSize = 1 << 7;
     float level = 0.0f;
-    juce::OwnedArray<SineOscillator> oscillators;
+
+    juce::AudioSampleBuffer sineTable;
+    juce::OwnedArray<WavetableOscillator> oscillators;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
 };
